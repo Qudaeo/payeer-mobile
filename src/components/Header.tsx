f323a9a601d89575cced5d07c12b9cca6108f6a4
Aleksey Pekhterev <qudaeo@yandex.ru>
2022-09-07 21:01:16 +0700

    add opacity animation to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {Animated, StyleSheet} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import LogoIcon from '../assets/svg/accountScreen/logo.svg';
 import HeaderButtons from './HeaderButtons';
@@ -7,11 +7,26 @@ import HeaderButtons from './HeaderButtons';
 const Header = () => {
   const insets = useSafeAreaInsets();
 
+  const fadeAnim = useRef(new Animated.Value(0.6)).current;
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
   return (
-    <View style={[{marginTop: insets.top + 15}, styles.container]}>
+    <Animated.View
+      style={[
+        {marginTop: insets.top + 15},
+        styles.container,
+        {opacity: fadeAnim},
+      ]}>
       <LogoIcon width={114.09} height={(114.09 * 26) / 127} />
       <HeaderButtons />
-    </View>
+    </Animated.View>
   );
 };
 
